89a609ca5fcfa2d846dd2eca43c018a959531b86
Eric <eric.basley@redpelicans.com>
2015-11-11 16:00:10 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Formo, Field, MultiField} from 'formo';
+
+const App = () => {
+  return (
+    <span>Formo Samples</span>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById("formo"));                                                                                      
+
+
+
